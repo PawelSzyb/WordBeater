160f914fc2780af9fea9066770782a3d7296bd1d
PawelSzyb <pawelszybaj@@wp.pl>
2018-08-08 13:20:35 +0200

    functions gameStatus, countdown and showWord

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-let time = currentLevel;
+window.addEventListener("load", init);
+
+let time = 5;
 let score = 0;
 let isPlaying;
 
@@ -37,3 +39,37 @@ const words = [
   "space",
   "definition"
 ];
+
+// Init Game
+function init() {
+  // Load random word
+  showWord(words);
+  // Decrement time every second
+  setInterval(countdown, 1000);
+  //Check if game is over
+  setInterval(checkGameStatus, 50);
+}
+
+function showWord(words) {
+  // Random word from an array
+  const randomIndex = Math.floor(Math.random() * words.length);
+  // Output random word
+  currentWord.innerHTML = words[randomIndex];
+}
+
+function countdown() {
+  // Check if time did not end
+  if (time > 0) {
+    time--;
+  } else {
+    // Game is over
+    isPlaying = false;
+  }
+  timeDisplay.innerHTML = time;
+}
+
+function checkGameStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "Game Over!";
+  }
+}
